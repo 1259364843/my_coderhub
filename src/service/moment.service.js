82509306e9a22ba9bc63604e1f22a7bb947ee1c5
sch <1259364843@qq.com>
2024-03-10 20:56:22 +0800

    update:查询动态详情新增label列表

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -38,36 +38,47 @@ class MomentService {
     const statement =
       `
       SELECT
-      m.id,
-      m.content,
-      m.createAt,
-      m.updateAt,
-      JSON_OBJECT( 'userId', u.id, 'userName', u.username, 'createTime', u.createAt, 'updateTime', u.updateAt ) user,
-      (
-        JSON_ARRAYAGG(
-          JSON_OBJECT(
-            'id',
-            c.id,
-            'content',
-            c.content,
-            'commentId',
-            c.comment_id,
-            'createTime',
-            c.createAt,
-            'updateTime',
-            c.updateAt,
-            'user', JSON_OBJECT('id', cu.id, 'userName', cu.username)
-          )) 
-      ) commentList 
-    FROM
-      moment m
+        m.id,
+        m.content,
+        m.createAt,
+        m.updateAt,
+        JSON_OBJECT( 'userId', u.id, 'userName', u.username, 'createTime', u.createAt, 'updateTime', u.updateAt ) USER,
+        (
+        SELECT
+          JSON_ARRAYAGG(
+            JSON_OBJECT(
+              'id',
+              c.id,
+              'content',
+              c.content,
+              'commentId',
+              c.comment_id,
+              'createTime',
+              c.createAt,
+              'updateTime',
+              c.updateAt,
+              'user',
+              JSON_OBJECT( 'id', cu.id, 'userName', cu.username ) 
+            )) 
+        FROM comment c
+        LEFT JOIN users cu ON c.user_id = cu.id 
+        WHERE
+          c.moment_id = m.id 
+        ) comments,
+        (
+          JSON_ARRAYAGG(JSON_OBJECT(
+            'id',l.id, 'name',l.name
+          ))
+        ) labels
+        
+      FROM moment m
       LEFT JOIN users u ON u.id = m.user_id
-      LEFT JOIN COMMENT c ON c.moment_id = m.id
-      LEFT JOIN users cu ON cu.id = c.user_id 
-    WHERE
-      m.id = ?
-    GROUP BY
-      m.id;
+      LEFT JOIN moment_label ml ON ml.moment_id = m.id
+      LEFT JOIN label l ON ml.label_id = l.id
+      WHERE
+        m.id = ?
+      GROUP BY
+        m.id;
       `
     const [res] = await connection.execute(statement, [momentId])
     return res
